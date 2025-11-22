145ad9834e0c4c00372e4c9c5c023ceb314dda82
benja <be.arandaa@duocuc.cl>
2025-11-22 20:43:49 -0300

    Initial commit: Estructura base Spring Boot

---
+++ backend-LevelUP/src/test/java/com/duoc/backend_LevelUP/BackendLevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.backend_LevelUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLevelUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
