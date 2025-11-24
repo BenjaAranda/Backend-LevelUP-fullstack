8ba99c3c50f3fd9f8646117c273809373e5e66c6
Joaquin <joaq.robles@duocuc.cl>
2025-11-24 14:58:12 -0300

    Usuario/admin listo

--- backend-LevelUP/src/main/java/com/duoc/backend_LevelUP/dto/RegisterRequest.java
+++ backend-LevelUP/src/main/java/com/duoc/backend_LevelUP/dto/RegisterRequest.java
@@ -1,15 +1,19 @@
 package com.duoc.backend_LevelUP.dto;
 
+import com.duoc.backend_LevelUP.models.Role;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
     private String nombre;
     private String email;
     private String password;
-    private Integer edad; // Opcional, según tu frontend
+    private Integer edad;
+    private Role role;
 }
